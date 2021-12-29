f7109650b8ffaf4ccd745cca91f134b33a703eec
Frank Xin <frankxin@macbook-air.myfiosgateway.com>
2021-12-29 02:01:36 -0500

    Changed scheduler interval

--- scheduler.py
+++ scheduler.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from scraper import reddit_keyboard_scraper
 scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('interval', minutes=10)
+@scheduler.scheduled_job('interval', minutes=3)
 def interval_job():
     reddit_keyboard_scraper()
     
