be69548c40093977531fb3c1653e8d0cfc02b545
Frank Xin <frankxin@macbook-air.myfiosgateway.com>
2021-12-29 02:23:09 -0500

    Changed scraping interval to 8 hours

--- scheduler.py
+++ scheduler.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from scraper import reddit_keyboard_scraper
 scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('interval', minutes=3)
+@scheduler.scheduled_job('interval', minutes=960)
 def interval_job():
     reddit_keyboard_scraper()
     
