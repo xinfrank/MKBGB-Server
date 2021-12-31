45509733ceffdef0401f228b7c6d4c9ed9f8de0d
Frank Xin <91763541+xinfrank@users.noreply.github.com>
2021-12-31 13:30:25 -0600

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,7 @@ from scraper import reddit_keyboard_scraper
 
 scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('interval', minutes=10)
+@scheduler.scheduled_job('interval', minutes=960)
 def interval_job():
     reddit_keyboard_scraper()
     
